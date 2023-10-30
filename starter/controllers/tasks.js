011ae05eedfc2cd928bf63c6d9840b03bdcf09cd
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-30 10:24:10 -0400

    Add update to the CRUD

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -35,12 +35,34 @@ const getTask = async (req, res) => {
 }  
 }
 
-const updateTask = (req, res) => {
-  res.send('update single task')
+const deleteTask = async (req, res) => {
+try {
+  const {id:taskID} = req.params;
+  const task = await Task.findOneAndDelete({_id:taskID})  
+  if(!task) {
+    return res.status(404).json({msg: `No task with id: ${taskID}`})
+  }
+  res.status(200).json({task})
+} catch (error) {
+  res.status(500).json({msg:error})
+ }
 }
 
-const deleteTask = (req, res) => {
-  res.send('delete single task')
+const updateTask = async (req, res) => {
+  try {
+  const {id: taskID} = req.params 
+  
+  const task = await Task.findOneAndUpdate({_id:taskID}, req.body)
+
+  if(!task) {
+    return res.status(404).json({msg: `No task with id: ${taskID}`})
+  }
+  
+  res.status(200).json({task})
+  } catch (error) {
+  res.status(500).json({msg:error})
+  }
+  
 }
 
 module.exports = {
