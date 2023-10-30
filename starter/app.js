ddab656daf697078f09a63009d8f8f2acd90955b
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-30 06:43:18 -0400

    Add single id filter and get all

--- starter/app.js
+++ starter/app.js
@@ -21,7 +21,7 @@ const port = 5000;
 const start =  async () => {
   try {
     await connectDB(process.env.MONGO_URI) // access secret variable en .env
-    app.listen(port, () => console.log(`Listenig on port ${port}...`))
+    app.listen(port, console.log(`Listenig on port ${port}...`))
 
   } catch(error) {
     console.log(error)
