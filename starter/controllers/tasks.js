eb03b7d76889c116e47c46aab5c254cd1e3c8b14
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-03 10:16:47 -0400

    Update sintaxys in controllers tasks

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -3,7 +3,13 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({})
+
     res.status(200).json({tasks})
+    //res.status(200).json({tasks, amount:task.length})
+    // res
+    // .status(200)
+    // .json({success:"success", data:{tasks, nbHits:tasks.length} })
+
   } catch (error) {
     res.status(500).json({msg:error})
   }
@@ -52,7 +58,10 @@ const updateTask = async (req, res) => {
   try {
   const {id: taskID} = req.params 
   
-  const task = await Task.findOneAndUpdate({_id:taskID}, req.body)
+  const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+    new:true, 
+    runValidators:true,
+  })
 
   if(!task) {
     return res.status(404).json({msg: `No task with id: ${taskID}`})
