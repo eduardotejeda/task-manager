979783f8c83ac2596083fe6d1370128a403d2915
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-08 09:40:58 -0400

    Add custom error messsage

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async') 
+const {createCustomError} = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper (async (req, res) => {
     const tasks = await Task.find({})
@@ -13,11 +14,12 @@ const createTask = asyncWrapper ( async (req, res) => {
   
 })
 
-const getTask = asyncWrapper ( async (req, res) => {
+const getTask = asyncWrapper ( async (req, res, next) => {
   const {id:taskID} = req.params
   const task = await Task.findOne({_id:taskID});    
   if(!task) {
-    return res.status(404).json({msg: `No task with id: ${taskID}`})
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
+    
   }
   res.status(200).json({task})
  
@@ -27,7 +29,7 @@ const deleteTask = asyncWrapper (async (req, res) => {
   const {id:taskID} = req.params;
   const task = await Task.findOneAndDelete({_id:taskID})  
   if(!task) {
-    return res.status(404).json({msg: `No task with id: ${taskID}`})
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
   }
   res.status(200).json({task})
 
@@ -43,7 +45,7 @@ const updateTask = asyncWrapper (async (req, res) => {
   })
 
   if(!task) {
-    return res.status(404).json({msg: `No task with id: ${taskID}`})
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
   }
   
   res.status(200).json({task})
--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -1,5 +1,11 @@
+const {CustomAPIError} = require('../errors/custom-error')
+
 const errorHandlerMiddleware = (err,req, res, next) => {
-  return res.status(500).json({msg:`Something went wrong, try again later`})
+  if(err instanceof CustomAPIError){
+    return res.status(err.statusCode).json({msg:err.message})
+  }
+  
+  return res.status(500).json({msg:'Something went wrong, please trya again'})
 }
 
 module.exports = errorHandlerMiddleware
